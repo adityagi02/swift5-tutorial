81696c456a2cf162dd7506fa46c1824f92b506a0
Aditya Tyagi <77538183+adityagi02@users.noreply.github.com>
2022-06-21 08:56:09 +0530

    Create code.swift

---
+++ Constants, Variables and Data Types/Code/code.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+
+// <---VARIABLE--->
+
+var age = 20;
+// print(age)
+age = 10;
+print(age);
+
+// <---CONSTANT--->
+
+let num = 20
+print(num)
+// num = 5
+print(num)
+
+
+// <---TYPE ANNOTATIONS--->
+
+var name: String = "Aditya Tyagi"
+var id:Int = 0126
+
+print(name)
+
+
+// This is a single line comment.
+
+/*
+ This is a
+ multi line comment.
+ */
+
+
+
+
+
+
